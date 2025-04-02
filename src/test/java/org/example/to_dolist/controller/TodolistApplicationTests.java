9729be26e9ea4ed407f3872e744beefec546fad5
SzynX <szynx7@gmail.com>
2025-04-02 20:10:04 +0200

    0.4

---
+++ src/test/java/org/example/to_dolist/controller/TodolistApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.to_dolist.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Ha az alkalmazás sikeresen elindul, akkor ez a teszt is sikeres lesz.
+		// A teszt akkor fut le sikeresen, ha az alkalmazás kontextusa megfelelően betöltődik.
+	}
+}
