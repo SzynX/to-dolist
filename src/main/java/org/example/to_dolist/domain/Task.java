2951099d51dd5aa183cb03af93e942c895bf4084
SzynX <szynx7@gmail.com>
2025-05-01 13:34:21 +0200

    0.938

--- src/main/java/org/example/to_dolist/domain/Task.java
+++ src/main/java/org/example/to_dolist/domain/Task.java
@@ -1,18 +1,22 @@
 package org.example.to_dolist.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
     private String title;
     private String description;
     private LocalDate dueDate;
