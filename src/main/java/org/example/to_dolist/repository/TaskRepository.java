2f537028d17c819281c8239e57db47bf32e9a726
SzynX <szynx7@gmail.com>
2025-05-10 10:55:35 +0200

    1.2.2 Kisebb hibajavitás & Home Gomb hozzáadása

--- src/main/java/org/example/to_dolist/repository/TaskRepository.java
+++ src/main/java/org/example/to_dolist/repository/TaskRepository.java
@@ -9,26 +9,14 @@ import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 
-    // Feladatok keresése felhasználó ID és
-    // archivált státusz alapján, rendezéssel
+
     List<Task> findByUserIdAndArchived(
             UUID userId, boolean archived, Sort sort);
 
-    // Feladatok keresése, amikhez nincs
-    // felhasználó rendelve ÉS
-    // archivált státusz alapján, rendezéssel
+
     List<Task> findByUserIsNullAndArchived(boolean archived, Sort sort);
 
-    // Minden feladat keresése
-    // archivált státusz alapján, rendezéssel
+
     List<Task> findByArchived(boolean archived, Sort sort);
 
-    // Az eredeti findByUserId(UUID userId, Sort sort) és
-    // findByUserIsNull(Sort sort)
-    // metódusokat valószínűleg már nem
-    // fogjuk közvetlenül használni a TaskService-ben,
-    // mivel mostantól mindig figyelembe
-    // vesszük az archivált státuszt.
-    // De a Service-ben lévő logika eldönti,
-    // melyik újat hívja.
 }
